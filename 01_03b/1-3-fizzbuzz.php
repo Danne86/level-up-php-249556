79d642431a5ed3d7dc1cf42b357e81f33a1fea8d
Daniel Lind <daniel.lindh86@hotmail.se>
2022-12-03 15:45:28 +0000

    solution fizzbuzz

--- 01_03b/1-3-fizzbuzz.php
+++ 01_03b/1-3-fizzbuzz.php
@@ -1,1 +1,20 @@
 <?php
+function output($i)
+{
+  $out = "";
+  if ($i % 3 == 0)
+    $out .= "Fizz";
+  if ($i % 5 == 0)
+    $out .= "Buzz";
+  if (strlen($out) > 0)
+    return "$out.<br>";
+  return "$i<br>";
+}
+?>
+<div>
+  <?php
+  for ($i = 1; $i <= 100; $i++) {
+    echo output($i);
+  }
+  ?>
+  <div>
